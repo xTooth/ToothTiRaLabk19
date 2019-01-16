b53bb1b2604e6fb3752c2b0e328fd456c69cca74
xTooth <jamsen.kim@gmail.com>
2019-01-16 19:08:42 +0200

    Basically blank gradle project
    ADDED>
        A new basically blank Java Gradle project.

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("gradle is working");
+        System.out.println("ready for whatever implementation");
+    }
+}
